7ad9f22d014864d6166c462d4c7b7af52d1ac720
Maria <flamik@inbox.ru>
2023-09-24 12:59:26 +0300

    first and second hw

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите число a ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число b ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+if (num_a>num_b)
+{
+Console.WriteLine("a>b");
+}
+else if (num_a<num_b)
+{
+Console.WriteLine("a<b"); 
+}
+else 
+{
+Console.WriteLine("a=b"); 
+}
---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ведите число a ");
+int num_a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число b ");
+int num_b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число c ");
+int num_c = Convert.ToInt32(Console.ReadLine());
+int max=num_a;
+if (num_b>max)
+{
+    max=num_b;
+}
+if (num_c>max)
+{
+    max=num_c;
+}
+Console.WriteLine("Максимальное число: " + max);
